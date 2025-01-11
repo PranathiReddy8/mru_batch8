e3ebde1edb3dc0aaff040b6ce430bd6062ebc9e0
PranathiReddy8 <2311cs010566@mallareddyuniversity.ac.in>
2025-01-11 10:29:48 +0530

    Demo3

---
+++ src/com/mru/oops/Test11.java
@@ -0,0 +1,9 @@
+package com.mru.oops;
+
+public class Test11 {
+String message  = "Princy";
+public void m1() {
+	System.out.println("Princy1");
+	
+}
+}
---
+++ src/com/mru/oops/Test12.java
@@ -0,0 +1,8 @@
+package com.mru.oops;
+
+public class Test12 {
+	String message = "Pranathi";
+	public void m2() {
+		System.out.println("Pranathi1");
+	}
+}
